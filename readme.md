acfd1d533b6e1424c8aa0ca8c98f842551abbe13
redbastie <redbastie@icloud.com>
2021-01-18 07:30:50 -0500

    add readme

---
+++ readme.md
@@ -0,0 +1,29 @@
+# Livewire Auto Route
+
+Automatic routes for your Livewire components.
+
+## Installation
+
+Install via composer:
+
+    composer require redbastie/livewire-auto-route
+
+## Usage
+
+Specify public `$route*` properties in your full page Livewire component:
+
+    class Vehicle extends \Livewire\Component
+    {
+        public $routeUri = '/vehicle/{name}';
+        public $routeDomain = null;
+        public $routeMiddleware = ['guest'];
+        public $routeName = 'vehicle';
+        public $routeWhere = ['name' => '[A-Za-z]+'];
+    
+        public function render()
+        {
+            return view('livewire.vehicle');
+        }
+    }
+
+A minimum of `$routeUri` is required.
--- src/Routes.php
+++ src/Routes.php
@@ -12,22 +12,22 @@ Route::middleware('web')->group(function () {
             $namespace = 'App\\Http\\Livewire\\' . str_replace(['/', '.php'], ['\\', ''], $file->getRelativePathname());
             $class = app($namespace);
 
-            if (property_exists($class, 'routeUri')) {
+            if (property_exists($class, 'routeUri') && $class->routeUri) {
                 $route = Route::get($class->routeUri, $namespace);
 
-                if (property_exists($class, 'routeDomain')) {
+                if (property_exists($class, 'routeDomain') && $class->routeDomain) {
                     $route->domain($class->routeDomain);
                 }
 
-                if (property_exists($class, 'routeMiddleware')) {
+                if (property_exists($class, 'routeMiddleware') && $class->routeMiddleware) {
                     $route->middleware($class->routeMiddleware);
                 }
 
-                if (property_exists($class, 'routeName')) {
+                if (property_exists($class, 'routeName') && $class->routeName) {
                     $route->name($class->routeName);
                 }
 
-                if (property_exists($class, 'routeWhere')) {
+                if (property_exists($class, 'routeWhere') && $class->routeWhere) {
                     $route->where($class->routeWhere);
                 }
             }
